59529eb49304fac52b387006fd9b7ad78e873244
Aarsh Bajaj <aarshbajaj22@gmail.com>
2025-03-27 08:56:49 -0400

    CCC 2012

---
+++ DmojProblems/2012-S2-AN.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <ctime>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <cstdio>
+#include <format>
+#include <initializer_list>
+#include <unordered_set>
+#include <cmath>
+
+using namespace std;
+#define pb push_back
+
+int main() {
+    unordered_map<char, int> m1 = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+    string numbers;
+    cin >> numbers;
+
+    int final = 0;
+    for (int i = 0; i < numbers.size(); i += 2) {
+        int a = numbers[i] - '0';           // Convert char to integer
+        int r = m1[numbers[i + 1]];          // Roman numeral base value
+        int pair_value = a * r;
+
+        // Check if the next Roman numeral has a higher base value
+        if (i + 3 < numbers.size() && m1[numbers[i + 3]] > r) {
+            final -= pair_value;             // Subtract if next Roman numeral is greater
+        } else {
+            final += pair_value;             // Otherwise, add
+        }
+    }
+
+    cout << final << endl;
+
+    return 0;
+}
