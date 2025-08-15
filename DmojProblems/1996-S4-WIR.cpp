653af2cb7d4899a1869b2b73a5a0de5b8b235f6f
Aarsh Bajaj <aarshbajaj22@gmail.com>
2025-08-15 17:04:10 -0400

    CCC 1996

--- DmojProblems/1996-S4-WIR.cpp
+++ DmojProblems/1996-S4-WIR.cpp
@@ -16,75 +16,65 @@ using namespace std;
 #define pb push_back
 
 unordered_map<char, int> romans = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+string romans[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+int numbers[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
-bool valid(char a, char b){
-    if(a=='I' && (b=='V' || b=='X')){
+bool valid(char a, char b) {
+    if(a == 'I' && (b == 'V' || b == 'X')) {
         return true;
-    }else if(a=='X' && (b=='L' || b=='C')){
+    } else if(a == 'X' && (b == 'L' || b == 'C')) {
         return true;
-    }else if(a=='C' && (b=='D' || b=='M')){
+    } else if(a == 'C' && (b == 'D' || b == 'M')) {
         return true;
     }
     return false;
 }
 
-int romanToInt(string s){
+int romanToInt(string s) {
     int ans = 0;
-    for(int i=0; i<int(s.size()); i++){
+    for(int i = 0; i < s.size() - 1; i++) {
         char a = s[i];
-        if(i<int(s.size())-1){
-            char b = s[i+1];
-            if(valid(a, b)){
-                ans+=romans[b]-romans[a];
-                i++;
-            }else{
-                ans+=romans[a];
-            }
-        }else{
-            ans+=romans[a];
+        char b = s[i + 1];
+        if(valid(a, b)) {
+            ans += (romans[b] - romans[a]);
+            i++;
+        } else {
+            ans += romans[a];
         }
     }
+}
+
+string intToRoman(int num) {
+    string ans;
+    for(int i = 0; i < 13; i++) {
+        if((num - numbers[i]) > 0) {
+            ans += romans[i];
+            num -= romans[i];
+        }
+    }
+
     return ans;
 }
 
-string intToRomans(int n){
-    string str_romans[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-
-    string result = "";
-
-    for (int i = 0; i < 13; ++i){
-        while(n - values[i] >= 0){
-            result += str_romans[i];
-            n -= values[i];
-        }
-    }
-    return result;
-}
-
 int main() {
-    int n;
+    int n; 
     cin >> n;
-    while(n--){
-        string s, f="", sec="";
-        bool found = false;
-        cin >> s;
-        for(int i=0; i<int(s.size()-1);i++){
-            if(found){
-                sec+=s[i];
-            }else{
-                if(s[i]=='+'){
-                    found = true;
-                    continue;
-                }
-                f+=s[i];
+    while(n--) {
+        string p, f, s;
+        cin >> p;
+        for(int i = 0; i < p.size() - 1; i++) {
+            if(p[i] != '+') {
+                f += p[i];
+            } else {
+                i++;
+                s += p[i];
             }
         }
-        cout << s;
-        int sum = romanToInt(f) + romanToInt(sec);
-        if(sum>1000) cout << "CONCORDIA CUM VERITATE" << endl;
-        else{
-            cout << intToRomans(sum) << endl;
+        int sum = romanToInt(s) + romanToInt(f);
+        if(sum > 1000) {
+            cout << "CONCORDIA CUM VERITATE" << endl;
+        } else {
+            cout << intToRoman(sum) << endl;
         }
     }
     return 0;
